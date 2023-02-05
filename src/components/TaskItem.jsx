aeebf7901636b8975fe207e4953dd3a55aa61901
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2023-02-05 21:58:47 +0400

    Creating add task form and displaying tasks in a list

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+// Импорт билбиотеки
+import { CheckIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/outline';
+
+// Стили
+import styles from './TaskItem.module.css';
+
+const TaskItem = ({task}) => {
+    const [isChecked, setIsChecked] = useState(task.checked);
+
+    const handleCheckboxChange = (e) => {
+        setIsChecked(!isChecked)
+    }
+
+    return (
+        <li className={styles.task}>
+            <div className={styles["task-group"]}>
+                <input
+                    type="checkbox"
+                    className={styles.checkbox}
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                    name={task.name}
+                    id={task.id}
+                />
+                <label 
+                    htmlFor={task.id}
+                    className={styles.label}
+                >
+                    {task.name}
+                    <p className={styles.checkmark}>
+                        <CheckIcon strokeWidth={2} width={24} height={24}/>
+                    </p>
+                </label>
+            </div>
+            <div className={styles["task-group"]}>
+                <button
+                    className='btn'
+                    aria-label={`Обнови ${task.name} задачу`}
+                    // onClick={}
+                >
+                    <PencilSquareIcon width={24} height={24}/>
+                </button>
+
+                <button
+                    className={`btn ${styles.delete}`}
+                    aria-label={`Удалить ${task.name} задачу`}
+                    // onClick={}
+                >
+                    <TrashIcon width={24} height={24}/>
+                </button>
+
+            </div>
+        </li>
+    )
+}
+
+export default TaskItem
